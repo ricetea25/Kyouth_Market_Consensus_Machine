a4296500d9428e19dd8f1dd9bae5301db67587a8
BOMBARDINO CROCODILO <ryan42cmp@gmail.com>
2026-06-11 03:15:32 +0800

    Chore: made call request more clear

--- backend/src/config.py
+++ backend/src/config.py
@@ -1,13 +1,16 @@
 from pydantic_settings import BaseSettings
 
+
 class Settings(BaseSettings):
     environment: str = "development"
     database_url: str = "sqlite:///./src/consensus.db"
     openai_api_key: str = ""
     gemini_api_key: str = ""
     alpha_vantage_api_key: str
+    frontend_origin: str = "http://localhost:3000"
 
     class Config:
         env_file = ".env"
+
 
 settings = Settings()
